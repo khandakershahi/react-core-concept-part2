70e275e7ee587a97646595e53718ab4eb3ddd00e
khandakershahi <khandakershahi@gmail.com>
2025-09-16 22:44:46 +0600

    post api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Batsman from './Batsman';
 import Users from './Users';
 import { Suspense } from 'react';
 import Friends from './Friends';
+import Posts from './Posts';
 
-const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+// const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
 
-const fetchFriends = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+// const fetchFriends = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//   return res.json();
+// }
+
+
+const fetchPosts = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   return res.json();
 }
 
 function App() {
 
-  const friendsPromise = fetchFriends();
+  // const friendsPromise = fetchFriends();
+  const postsPromise = fetchPosts();
 
   function handleclick() {
     alert('I am click');
@@ -35,13 +43,17 @@ function App() {
 
       <h3>Vite + React</h3>
 
+      <Suspense fallback={<h4>Post are coming....</h4>}>
+        <Posts postsPromise={postsPromise}></Posts>
+      </Suspense>
+
       {/* <Suspense fallback={<h3>Loading....</h3>}>
         <Users fetchUsers={fetchUsers}></Users>
       </Suspense> */}
 
-      <Suspense fallback={<h2>Friends are coming for treat....</h2>}>
+      {/* <Suspense fallback={<h2>Friends are coming for treat....</h2>}>
         <Friends friendsPromise={friendsPromise}></Friends>
-      </Suspense>
+      </Suspense> */}
 
 
 
---
+++ src/Posts.jsx
@@ -0,0 +1,16 @@
+import { use } from "react";
+import Post from "./Post";
+
+export default function Posts({ postsPromise }) {
+
+    const posts = use(postsPromise);
+    console.log(posts)
+    return (
+        <div className="card">
+            <h2>All the posts are here: {posts.length}</h2>
+            {
+                posts.map(post => <Post key={posts.id} post={post}></Post>)
+            }
+        </div>
+    )
+}
