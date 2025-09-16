1da08a3c8e50e638c843cbd4a3f75a1b63808494
khandakershahi <khandakershahi@gmail.com>
2025-09-16 18:12:27 +0600

    default

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import Counter from './Counter';
+import './App.css'
+import Batsman from './Batsman';
+
+
+function App() {
+
+  function handleclick() {
+    alert('I am click');
+  }
+
+  const handleClick3 = () => {
+    alert('clicked 3')
+  }
+
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum)
+  }
+
+  return (
+    <>
+
+      <h3>Vite + React</h3>
+
+      <Batsman></Batsman>
+
+      <Counter></Counter>
+
+
+      {/* <button onclick="handleClick()">Click Me</button> */}
+      <button onClick={handleclick}>Click Me</button>
+      <button onClick={function handleClick2() {
+        alert('Clicked 2')
+      }}>Click Me2</button>
+
+      <button onClick={handleClick3}>Click Me 3</button>
+      <button onClick={() => {
+        alert('click me 4')
+      }}>Click Me 4</button>
+
+      <button onClick={() => handleAdd5(10)}>click add 5</button>
+
+
+
+    </>
+  )
+}
+
+export default App
