3f49aae24c18f6a23055a0e174e363836515028b
khandakershahi <khandakershahi@gmail.com>
2025-09-16 22:08:30 +0600

    learn  fetech

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Counter from './Counter';
 import './App.css'
 import Batsman from './Batsman';
+import Users from './Users';
+import { Suspense } from 'react';
+import Friends from './Friends';
 
+const fetchUsers = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+
+const fetchFriends = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  return res.json();
+}
 
 function App() {
+
+  const friendsPromise = fetchFriends();
 
   function handleclick() {
     alert('I am click');
@@ -22,6 +34,16 @@ function App() {
     <>
 
       <h3>Vite + React</h3>
+
+      {/* <Suspense fallback={<h3>Loading....</h3>}>
+        <Users fetchUsers={fetchUsers}></Users>
+      </Suspense> */}
+
+      <Suspense fallback={<h2>Friends are coming for treat....</h2>}>
+        <Friends friendsPromise={friendsPromise}></Friends>
+      </Suspense>
+
+
 
       <Batsman></Batsman>
 
